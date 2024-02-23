cd76385b2a1e31214278c5ee01374843f9ed00a9
Ezra Richard <125721936+ezrarichard@users.noreply.github.com>
2024-02-24 00:03:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,5 +63,5 @@ if st.button('Predict'):
     # Use st.markdown to display the HTML content
     st.markdown(html_content, unsafe_allow_html=True)
 
-st.markdown('<p style="position: fixed; bottom: 10px; right: 10px; color: gray; font-size: smaller;">Created by Ezra Richard</p>', unsafe_allow_html=True)
+st.markdown('<p style="position: fixed; bottom: 10px; left: 10px; color: gray; font-size: smaller;">Created by Ezra Richard</p>', unsafe_allow_html=True)
 
